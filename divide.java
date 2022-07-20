837a73401288b88f12d8ec6bd4a416ea22e6806a
Aadi <109529147+AjayBhardwaj-Aadi@users.noreply.github.com>
2022-07-20 16:38:03 +0530

    Add files via upload

---
+++ divide.java
@@ -0,0 +1,12 @@
+class A
+{
+public static void main(String args[])
+{
+int a=4;
+int b=2;
+System.out.println("Addition of A and B =" + (a+b));
+System.out.println("division of A and B ="+   (a/b));
+System.out.println("After subtraction of A and B ="+(a-b));
+System.out.println("After multiply ="+(a*b));
+}
+}
