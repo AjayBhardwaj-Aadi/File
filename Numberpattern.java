00fb33e3d1dc0117e20ed5a6a8c323df019754ec
Aadi <109529147+AjayBhardwaj-Aadi@users.noreply.github.com>
2022-07-27 19:28:42 +0530

    Add files via upload

---
+++ Numberpattern.java
@@ -0,0 +1,18 @@
+class Pattern
+{
+public static void main (String args[])
+{
+int  count=1;
+int n=6;
+for (int i =0;i<5;i++)
+{
+for (int j=0;j<=1;j++)
+{
+System.out.print(count);
+count++;
+System.out.print(" ");
+}
+System.out.println();
+}
+}
+}
